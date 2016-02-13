1f068fc53c3f34f4a25e2ad437d84927ef2a8977
John Reese <jpreese@mtu.edu>
2016-02-13 13:35:25 -0500

    initial commit

---
+++ Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singletonOne = Singleton.GetInstance();
+            var singletonTwo = Singleton.GetInstance();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+}
